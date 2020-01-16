6dd4cc737fc8f307d9957f94ea67cc4b36ca3fe0
bryanlai00 <bryanjl22@hotmail.com>
2020-01-15 20:20:45 -0500

    Find next player to move, find empty indexes and find children

--- ttt.py
+++ ttt.py
@@ -1,4 +1,6 @@
 import sys
+import random
+import copy
 
 class BoardNode:
     def __init__(self,layout):
@@ -16,6 +18,23 @@ class BoardNode:
                 ret_string += "\n"
             ret_string += " " + self.layout[i]
         return ret_string + "\n"
+
+def player_move(board):
+    run = True
+    while run:
+        move = input("Please select a position to place \'X\' (0-8): ")
+        try:
+            move = int(move)
+            if (move >= 0 and move <= 8):
+                if(is_empty(board,move)):
+                    run = False
+                    board.layout[move] = 'x'
+                else:
+                    print("This space is occupied.")
+            else:
+                print("This is not a valid number.")
+        except:
+            print("Please type a number.")
 
 def is_winner(board, symbol):
     return ((board.layout[6] == symbol and board.layout[7] == symbol and board.layout[8] == symbol) or # across the top
@@ -40,12 +59,67 @@ def is_empty(board, index):
 def check_wld(board):
     if(is_winner(board,'x')):
         print("x wins and o loses.")
+        board.final_state = 'x'
+        return 3
     elif(is_winner(board,'o')):
         print("o Wins and x loses.")
+        board.final_state = 'o'
+        return 2
     elif(is_full(board)):
         print("draw!")
+        board.final_state = 'd'
+        return 1
     else:
-        print("Board not finished yet!")
+        emptyIndexes = []
+        nextMove = None
+        x_count = 0
+        o_count = 0
+        #Find ratio between the amount of x's and o's to find whose turn it is.
+        for i in board.layout:
+            if i == 'x':
+                x_count += 1
+            if i == 'o':
+                o_count += 1
+        if(x_count == o_count):
+            nextMove = 'x'
+        else:
+            nextMove = 'o'
+
+        #Fill empty array.
+        for spot in range(len(board.layout)):
+            if(is_empty(board,spot)):
+                emptyIndexes.append(spot)
+        print("Empty Indexes: " + str(emptyIndexes))
+        #Finding children.
+        for idx in emptyIndexes:
+            temp = copy_board(board)
+            temp.layout[idx] = nextMove
+            temp.moveMade = idx
+            board.children.append(temp)
+        print("Board Children: " + str(board.children))
+
+
+#copies board for recursion:
+def copy_board(board):
+    layout =[0,0,0,0,0,0,0,0,0]
+    for i in range(0,9):
+        layout[i] = board.layout[i]
+    return BoardNode(layout)
+
+#Gets best move/computer AI
+def best_move(board, symbol):
+    if(is_winner(board,'x')):
+        print("x wins and o loses.")
+        board.final_state = 'x'
+        return 3
+    elif(is_winner(board,'o')):
+        print("o Wins and x loses.")
+        board.final_state = 'o'
+        return 2
+    elif(is_full(board)):
+        print("draw!")
+        board.final_state = 'd'
+        return 1
 
 if __name__ == "__main__":
     args = sys.argv
@@ -58,4 +132,5 @@ if __name__ == "__main__":
         print(board)
     else:
         print("Board Layout is incorrect.")
+
     check_wld(board)
